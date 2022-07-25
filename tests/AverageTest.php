6cbd4c4e9c722de3c155ef490907066995a07747
Matthew Obert <matt@hotwebmatter.com>
2022-07-25 18:16:08 +0000

    Fix PHPUnit\Framework\TestCase::setUp error.

--- tests/AverageTest.php
+++ tests/AverageTest.php
@@ -7,18 +7,18 @@ class AverageTest extends TestCase
 {
     protected $Average;
 
-    public function setUp()
+    public function setUp(): void 
     {
         $this->Average = new Average();
     }
 
-    public function testCalculationOfMean()
+    public function testCalculationOfMean(): void 
     {
         $numbers = [3, 7, 6, 1, 5];
         $this->assertEquals(4.4, $this->Average->mean($numbers));
     }
 
-    public function testCalculationOfMedian()
+    public function testCalculationOfMedian(): void 
     {
         $numbers = [3, 7, 6, 1, 5];
         $this->assertEquals(5, $this->Average->median($numbers));
